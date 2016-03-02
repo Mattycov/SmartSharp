4caa7196db48550d31e3edc51d013f98c765e938
mattycov <gillian1>
2016-03-02 22:51:28 +0000

    Initial commit

---
+++ Smart.Sharp/Smart.Sharp.Cli/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Smart.Sharp.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string smartRemotePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "smart");
+
+            SmartRemote smartRemote = new SmartRemote(smartRemotePath);
+
+            IntPtr smart = smartRemote.SpawnClient(@"C:\Program Files (x86)\Java\jre1.8.0_65\bin\java.exe", smartRemotePath, "http://world37.runescape.com/", "", 800, 600, null, null, null, null);
+
+            Console.WriteLine("Press any key to kill SMART");
+            Console.ReadKey();
+
+            bool ret = smartRemote.KillClient(smartRemote.GetClientPID(smart));
+
+            Console.WriteLine("SNART shutdown: " + ret);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Smart.Sharp/Smart.Sharp/Kernel32.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Smart.Sharp
+{
+    public class Kernel32
+    {
+
+        [DllImport("kernel32.dll")]
+        internal static extern IntPtr LoadLibrary(string dllPath);
+
+        [DllImport("kernel32.dll")]
+        internal static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        [DllImport("kernel32.dll")]
+        internal static extern bool FreeLibrary(IntPtr hModule);
+
+    }
+}
